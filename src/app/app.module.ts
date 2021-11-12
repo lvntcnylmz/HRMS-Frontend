af1c4953c814bc211ff2acb4a14eaddf2df1f741
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2021-11-12 21:25:49 +0100

    updated dir path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NaviComponent } from './components/navi/navi/navi.component';
+import { NaviComponent } from './components/navi/navi.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NaviComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NaviComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
