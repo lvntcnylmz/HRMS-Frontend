5e1feaf6bbec9b128179c25f4f7c5b0f0d52e757
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2021-11-13 13:13:57 +0100

    updated import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NaviComponent } from './components/navi/navi.component';
+import { JobAdvertisementComponent } from './components/job-advertisement/job-advertisement.component';
 
 @NgModule({
-  declarations: [AppComponent, NaviComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, NaviComponent, JobAdvertisementComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
