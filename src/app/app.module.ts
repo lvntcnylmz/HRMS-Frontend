3a6091f199a0e610110c504a7c23c69e00fab006
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2021-11-15 12:34:12 +0100

    updated files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NaviComponent } from './components/navi/navi.component';
 import { JobAdvertisementComponent } from './components/job-advertisement/job-advertisement.component';
+import { JobAdvertisementDetailComponent } from './components/job-advertisement-detail/job-advertisement-detail.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CategoryComponent } from './components/category/category.component';
 
 @NgModule({
-  declarations: [AppComponent, NaviComponent, JobAdvertisementComponent],
+  declarations: [AppComponent, NaviComponent, JobAdvertisementComponent, JobAdvertisementDetailComponent, FooterComponent, CategoryComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
