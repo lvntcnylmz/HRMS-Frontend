61a0bbb14e0dfb84614fb72461191f8f915c645b
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2021-11-27 12:02:10 +0100

    refactore files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
+import { CategoryComponent } from './components/category/category.component';
 import { HomeComponent } from './components/home/home.component';
 import { EmployerComponent } from './components/employer/employer.component';
 import { JobAdvertisementComponent } from './components/job-advertisement/job-advertisement.component';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { LoginComponent } from './components/login/login.component';
@@ -9,7 +10,11 @@ import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
-  { path: 'jobs', component: JobAdvertisementComponent },
+  {
+    path: 'jobs',
+    component: JobAdvertisementComponent,
+  },
+  { path: 'jobs', component: CategoryComponent },
   { path: 'jobs/:jobId', component: JobAdvertisementComponent },
   { path: 'companies', component: EmployerComponent },
   { path: 'about', component: AboutUsComponent },
