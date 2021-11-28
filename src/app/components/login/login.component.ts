0b2249afb43f38d487c3360950a24ceb85532be5
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2021-11-28 17:11:17 +0100

    added new components

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
+    this.createLoginForm();
   }
 
+  createLoginForm() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      // let userModel = Object.assign({}, this.loginForm.value);
+      // this.userService.add(userModel).subscribe((response) => {
+      //   console.log(response);
+      //   console.log('Success');
+      // });
+      // } else {
+      //   console.log('Error');
+    }
+  }
 }
