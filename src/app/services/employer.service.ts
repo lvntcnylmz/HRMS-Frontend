b0ddbe2e738a719b20422f402b229feebd6bdfb1
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2021-12-20 14:53:51 +0100

    chore : add service

--- src/app/services/employer.service.ts
+++ src/app/services/employer.service.ts
@@ -1,3 +1,5 @@
+import { SingleResponseModel } from './../models/singleResponseModel';
+import { ResponseModel } from './../models/responseModel';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -15,5 +17,10 @@ export class EmployerService {
   getEmployers(): Observable<ListResponseModel<Employer>> {
     let newPath = this.apiURL + 'employers/getAll';
     return this.httpClient.get<ListResponseModel<Employer>>(newPath);
+  }
+
+  getEmployerById(id: number): Observable<SingleResponseModel<Employer>> {
+    let newPath = this.apiURL + 'employers/getById/' + id;
+    return this.httpClient.get<SingleResponseModel<Employer>>(newPath);
   }
 }
