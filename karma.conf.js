9dd37dee41c988103c562b6360b2c7773163554b
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2022-05-22 22:34:17 +0200

    added custom launcher for tests

--- karma.conf.js
+++ karma.conf.js
@@ -39,6 +39,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadlessCI', 'Firefox'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
